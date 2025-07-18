590f9c40523b79bcec460e70b52556ab4bfed5e3
velu97 <136069658+velu97@users.noreply.github.com>
2025-07-19 00:24:55 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -157,6 +157,7 @@ module "eks" {
 # OIDC provider for IRSA
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_name
+  depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "cluster" {

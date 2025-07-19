c5e722662c9e0f86df504fdd9da040efe18f2a98
velu97 <136069658+velu97@users.noreply.github.com>
2025-07-19 19:14:24 +0530

    Create alb.tf

---
+++ Interview/alb.tf
@@ -0,0 +1,44 @@
+resource "aws_lb" "app" {
+  name               = "prod-app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = module.vpc.public_subnets
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "prod-app-alb"
+  }
+}
+
+resource "aws_lb_target_group" "app" {
+  name        = "prod-app-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = module.vpc.vpc_id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+
+  tags = {
+    Name = "prod-app-tg"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
